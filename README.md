887e55dc565ea22abb9e0678910db87281648dac
Taco Ekkel <taco@q42.nl>
2018-03-18 18:23:04 +0100

    updateje

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# op1go
+# OP1GO
+
+Backup your OP-1 on the go with this code and a Raspberry Pi Zero.
+
+## How to get started
+
+* Setup your Pi and SD card (see below)
+* `sudo apt-get update`
+* `sudo apt-get install python3-pip git`
+* `git clone https://github.com/tacoe/op1go`
+* `pip3 install pyusb`
+
+## Setup Pi/SD card
+
+AKA the 2018 way to setup a Raspberry Pi, from a Mac, for SSH-ing over USB (no keyboard/display needed)
+
+* Get a micro-SD card. You'll need about 3Gb for the system, the rest is available for OP-1 backups.
+* Burn Raspbian Stretch Lite (Etcher is an easy and foolproof way) on it.
+* Re-insert the card so the 'boot' partition gets mounted
+* In a terminal:
+  * `cd /Volumes/boot`
+  * `sudo touch ssh`
+  * `sudo nano config.txt` -- to the bottom of the file, add `dtoverlay=dwc2` and save
+  * `sudo nano cmdline.txt` -- after `rootwait`, add `modules-load=dwc2,g_ether`, save
+* Eject the card
+* Start the Raspberry Pi while connected to your Mac's USB
+* Connect to it using `ssh pi@raspberrypi.local` and the default password (`raspberry`).
---
+++ op1go.py
@@ -0,0 +1,115 @@
+#! /usr/bin/python
+
+import os
+import sys
+import re
+import time
+import usb.core
+import usb.util
+import hashlib
+import subprocess
+import shutil
+import ctypes
+from datetime import datetime
+
+VENDOR_TE = 0x2367
+PRODUCT_OP1 = 0x0002
+USBID_OP1 = "*Teenage_OP-1*"
+OP1_BASE_DIRS = set(['tape', 'album', 'synth', 'drum'])
+MOUNT_DIR = "/media/op1"
+BACKUP_DIR_FORMAT = "%Y-%m-%d (%H%M%S)"
+HOME = os.path.join(os.getenv("HOME"), "op1go-backups")
+BACKUPS_DIR = os.path.join(HOME, "backups")
+
+# OP-1 connection
+def ensure_connection():
+  if not is_connected():
+    print("Connect your OP-1 and put it in DISK mode (Shift+COM -> 3)...")
+    wait_for_connection()
+
+def is_connected():
+  return usb.core.find(idVendor=VENDOR_TE, idProduct=PRODUCT_OP1) is not None
+
+def wait_for_connection():
+  try:
+    while True:
+      time.sleep(1)
+      if is_connected():
+        break
+  except KeyboardInterrupt:
+    sys.exit(0)
+
+# mounting
+def mountdevice(source, target, fs, options=''):
+  ret = ctypes.CDLL('libc.so.6', use_errno=True).mount(source, target, fs, 0, options)
+  if ret < 0:
+    errno = ctypes.get_errno()
+    raise RuntimeError("Error mounting {} ({}) on {} with options '{}': {}".
+     format(source, fs, target, options, os.strerror(errno)))
+
+def unmountdevice(target):
+  ret = ctypes.CDLL('libc.so.6', use_errno=True).umount(target, None)
+  if ret < 0:
+    errno = ctypes.get_errno()
+    raise RuntimeError("Error unmounting {}: {}".
+     format(target, os.strerror(errno)))
+
+def getmountpath():
+  o = os.popen('readlink -f /dev/disk/by-id/' + USBID_OP1).read()
+  if o.contains(USBID_OP1):
+    raise RuntimeError("Error getting OP-1 mount path: {}".format(o))
+  else
+    return o.rstrip()
+
+# copying
+def forcedir(path):
+  if not os.path.isdir(path):
+    os.makedirs(path)
+
+def get_visible_folders(d):
+  return list(filter(lambda x: os.path.isdir(os.path.join(d, x)), get_visible_children(d)))
+
+def get_visible_children(d):
+  return list(filter(lambda x: x[0] != '.', os.listdir(d)))
+
+def copytree(src, dst, symlinks=False, ignore=None):
+  for item in os.listdir(src):
+    s = os.path.join(src, item)
+    d = os.path.join(dst, item)
+    if os.path.isdir(s):
+      shutil.copytree(s, d, symlinks, ignore)
+    else:
+      shutil.copy2(s, d)
+            
+def backup_files(source, destination): 
+  dstroot = os.path.join(destination, datetime.now().strftime(BACKUP_DIR_FORMAT))
+  forcedir(dstroot)
+  for node in get_visible_children(source):
+    src = os.path.join(mount, node)
+    dst = os.path.join(dstroot, node)
+    forcedir(dst)
+    print("from: %s" % src)
+    print("to: %s" % dst)
+    #copytree(src, dst)
+
+####################################################################################
+# create mount point and local backup folders
+forcedir(BACKUPS_DIR)
+forcedir(MOUNT_DIR)
+
+# wait until OP-1 is connected
+print("")
+ensure_connection()
+
+# mount OP-1
+mountpath = getmountpath()
+print("Mountpath: %s" % mountpath)
+mountdevice(mountpath, MOUNT_DIR, 'ext 4', 'rw')
+
+# copy files to local storage
+backup_files(MOUNT_DIR, BACKUPS_DIR)
+
+# unmount OP-1
+unmountdevice(MOUNT_DIR)
+
+
